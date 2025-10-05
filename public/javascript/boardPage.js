41abcfadc188f820c761bee2002d8fc3f0aaaeef
Kamilly Vitoria Rodrigues Silva <kamillyr93@gmail.com>
2025-10-05 01:10:29 -0300

    Terminar de estilizar as colunas e sidebar, começar a fazer as funções em geral

--- public/javascript/boardPage.js
+++ public/javascript/boardPage.js
@@ -1,19 +1,34 @@
-const menuLinks = document.querySelectorAll("ul .selection-menu")
+const menuLinksSelection = document.querySelectorAll("ul .selection-aside")
+const categoryBoardSelection = document.querySelectorAll(".category")
+
+
+
 
 // Função de seleção dos links do menu lateral
 
-menuLinks.forEach(item => {
+menuLinksSelection.forEach(item => {
 
     // adicionado evento de clique para cada item do link
     item.addEventListener("click", () => {
 
         // para cada link do menu estou removendo a classe select
-        menuLinks.forEach(i => {
+        menuLinksSelection.forEach(i => {
             i.classList.remove("selected");
         });
 
         // depois estou colocando elas de novo
         item.classList.add("selected");
+    });
+});
+
+categoryBoardSelection.forEach(item => {
+    item.addEventListener("click", () => {
+        
+        categoryBoardSelection.forEach(i =>{
+            i.classList.remove("selectedCategory");
+        });
+
+        item.classList.add("selectedCategory");
     });
 });
 
